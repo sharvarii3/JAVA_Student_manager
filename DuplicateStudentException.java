a6a3c83be3dbd22f8e10d50e0f67faf3dc6e6e3f
sharvarii3 <168175435+sharvarii3@users.noreply.github.com>
2025-04-20 21:59:06 +0530

    Add files via upload

---
+++ DuplicateStudentException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+// DuplicateStudentException.java
+public class DuplicateStudentException extends Exception {
+  public DuplicateStudentException(String message) {
+    super(message);
+  }
+}
