92c5dc09237566ff140f8ef4b3b0751cdd037b0e
sharvarii3 <168175435+sharvarii3@users.noreply.github.com>
2025-04-20 21:59:45 +0530

    Add files via upload

---
+++ InvalidDataException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+// InvalidDataException.java
+public class InvalidDataException extends Exception {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
